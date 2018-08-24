3b6efe5e5bfc4a7aed1cf36ac039294642c6ff5a
Jeremy Tweddle <jeremy@breams.net>
2018-08-24 13:11:43 -0400

    Added custom history folder setting – defaults to `.history`

--- src/history.settings.ts
+++ src/history.settings.ts
@@ -94,6 +94,7 @@ export class HistorySettings {
             exclude =  <string[]>config.get('exclude'),
             historyPath,
             absolute,
+            folderName = <string>config.get('folderName'),
             message = '';
 
         if (typeof enabled === 'boolean')
@@ -146,11 +147,11 @@ export class HistorySettings {
                         absolute = true;
                     historyPath = path.join (
                         historyPath,
-                        '.history');
+                        folderName);
                     } else if (workspacefolder) {
                         historyPath = path.join (
                             historyPath,
-                            '.history',
+                            folderName,
                             (historyWS && this.pathIsInside(workspacefolder.fsPath, historyWS.fsPath) ? '' : path.basename(workspacefolder.fsPath))
                         );
                     }
@@ -161,7 +162,7 @@ export class HistorySettings {
                 absolute = false;
                 historyPath = path.join(
                     workspacefolder.fsPath,
-                    '.history'
+                    folderName
                 );
             }
         }
